b47ea485139fa565080ac6553e3a22541941f31e
EMMANUEL NEEQUAYE <eneequaye006@st.ug.edu.gh>
2025-08-03 11:56:11 +0000

    Implement inheritance and method overriding demo with Animal, Dog, and Cat classes

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Inheritance and Method Overriding Demo ---");
+            
+            Animal genericAnimal = new Animal();
+            Dog myDog = new Dog();
+            Cat myCat = new Cat();
+
+            Console.WriteLine("Animal says: ");
+            genericAnimal.MakeSound();
+            
+            Console.WriteLine("Dog says: ");
+            myDog.MakeSound();
+            
+            Console.WriteLine("Cat says: ");
+            myCat.MakeSound();
+        }
+    }
+
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
